442c6ad5c55e06e674de789d979bb7732a5427c3
Mikko Kokkoniemi <mikko.kokkoniemi@meiko.fi>
2017-10-22 22:32:48 +0300

    added arcs to simulate traffic lines

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { geoMercator, geoPath, geoCentroid } from 'd3-geo';
 import { select } from 'd3-selection';
 import { queue } from 'd3-queue';
 import * as topojson from 'topojson-client';
+import * as R from 'ramda';
 import jsonWorldMap from './countries.quantized.topo.json';
+import jsonFinland from './finland.json';
 
 // Obs! select is a dom-method
 
@@ -20,7 +22,7 @@ const svg = select('#map')
 let group = svg.append('g');
 
 
-const loaded = (countries) => {
+const loaded = (countries, traffic) => {
     // load map data to array
     geoData = topojson.feature(countries, countries.objects.countries).features;
     // append centroids for each country
@@ -49,8 +51,34 @@ const loaded = (countries) => {
        .attr('r', '2')
        .exit();
 
+    // draw arcs for yearly data
+    traffic.map(year => {
+      R.keys(year).map(i => {
+        year[i].map(country => {
+          const fromCountry = R.find(R.pathEq(['properties', 'NAME'], country.country))(geoData);
+          const toCountry = R.find(R.pathEq(['properties', 'NAME'], 'Finland'))(geoData);
+          const coordinates = [
+            fromCountry.centroid,
+            toCountry.centroid
+          ];
+
+          let line = svg.append('path')
+            .datum(coordinates)
+            .attr('d', c => {
+              const d = {source: c[0], target: c[1]};
+              const dx = d.target[0] - d.source[0];
+              const dy = d.target[1] - d.source[1];
+              const dr = Math.sqrt(dx * dx + dy * dy);
+              return `M${d.source[0]},${d.source[1]}A${dr},${dr} 0 0,1 ${d.target[0]},${d.target[1]}`;
+            })
+            .attr('class', 'arc')
+            .exit();
+        });
+      });
+    });
+
 }
 
-loaded(jsonWorldMap);
+loaded(jsonWorldMap, jsonFinland);
 
 
