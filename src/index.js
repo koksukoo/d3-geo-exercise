d87d30a002490b28e3c1cb8b4c0dd7bb7fe5747b
Mikko Kokkoniemi <mikko.kokkoniemi@meiko.fi>
2017-10-25 14:55:20 +0300

    fix whirlpool effect in arcs

--- src/index.js
+++ src/index.js
@@ -66,10 +66,15 @@ const drawMap = (countries, traffic) => {
           let line = svg.append('path')
             .datum(coordinates)
             .attr('d', c => {
+              const bend = 1.3;
               const d = {source: c[0], target: c[1]};
               const dx = d.target[0] - d.source[0];
               const dy = d.target[1] - d.source[1];
-              const dr = Math.sqrt(dx * dx + dy * dy);
+              const dr = Math.sqrt(dx * dx + dy * dy)*bend;
+              // to avoid whirlpool effect do a flip if needed
+              if (d.target[0] - d.source[0] < 0) {
+                return `M${d.target[0]},${d.target[1]}A${dr},${dr} 0 0,1 ${d.source[0]},${d.source[1]}`;
+              }
               return `M${d.source[0]},${d.source[1]}A${dr},${dr} 0 0,1 ${d.target[0]},${d.target[1]}`;
             })
             .attr('class', 'arc')
